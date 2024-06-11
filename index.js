ed5f1258709f4d3d63595862b13a8b40319196ea
Helen Varley <helen.varley@sopheon.com>
2024-06-11 21:29:57 +0100

    config to deploy to vercel

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
-require('dotenv').config();
-
 const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const mongoString = process.env.DATABASE_URL;
 
-mongoose.connect(mongoString);
-const database = mongoose.connection;
+const app = express()
+const PORT = 8000
 
-database.on('error', (error) => {
-    console.log(error)
+app.get('/', (req, res) => {
+  res.send('Hello World')
 })
 
-database.once('connected', () => {
-    console.log('Database Connected');
+app.get('/about', (req, res) => {
+  res.send('About route 🎉 ')
 })
-const app = express();
 
-app.use(cors());
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+})
+
+// require('dotenv').config();
+
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const mongoString = process.env.DATABASE_URL;
+
+// mongoose.connect(mongoString);
+// const database = mongoose.connection;
+
+// database.on('error', (error) => {
+//     console.log(error)
+// })
+
+// database.once('connected', () => {
+//     console.log('Database Connected');
+// })
+// const app = express();
+
+// app.use(cors());
   
-app.use(express.json());
+// app.use(express.json());
 
-app.listen(3005, () => {
-    console.log(`Server Started at ${3005}`)
-})
+// app.listen(3005, () => {
+//     console.log(`Server Started at ${3005}`)
+// })
 
-const routes = require('./routes/routes');
+// const routes = require('./routes/routes');
 
-app.use('/api', routes)
+// app.use('/api', routes)
